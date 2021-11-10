a13adeacf018b93f5bfd46062c232df8c986338b
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-10 12:27:54 +0900

    新規追加

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <form action="index.php" method="post">
+ <input type="text" name="name" class="form-text" value="">
+ <input class="contact-submit" type="submit" value="検索" />
+ </form>
+ <?php
+ echo '<p>こんにちは世界</p>';
+ echo var_dump(isset($_POST["name"]));
+ ?>
+</body>
+</html>
